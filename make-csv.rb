aa18eea47228bbf07f73e01ffcb6527afa44a273
Oliver Kurmis <oliver.kurmis@umwerk.net>
2016-06-07 11:24:39 +0200

    split firstname and last name

--- make-csv.rb
+++ make-csv.rb
@@ -2,8 +2,15 @@ require 'ffaker'
 
 line = ''
 delimiter = ';'
-line << FFaker::Name.name       #=> "Christophe Bartell"
+# line << FFaker::Name.name       #=> "Christophe Bartell"
+# line << delimiter
+
+line << FFaker::Name.first_name       #=> "Christoph"
 line << delimiter
+
+line << FFaker::Name.last_name       #=> "Bartell"
+line << delimiter
+
 line << FFaker::Internet.email  #=> "kirsten.greenholt@corkeryfisher.info"
 
 puts line
