8ea246c6dad651eac2f67fe5a83769617114f7ec
Oliver Kurmis <oliver.kurmis@umwerk.net>
2016-06-07 11:44:10 +0200

    switch to German names

--- make-csv.rb
+++ make-csv.rb
@@ -6,16 +6,14 @@ i = 1
 
 
 while i <= 5 do
-    # line << FFaker::Name.name       #=> "Christophe Bartell"
-    # line << delimiter
 
     line << i.to_s
     line << delimiter
 
-    line << FFaker::Name.first_name       #=> "Christoph"
+    line << FFaker::NameDE.first_name       #=> "Christoph"
     line << delimiter
 
-    line << FFaker::Name.last_name       #=> "Bartell"
+    line << FFaker::NameDE.last_name       #=> "Bartell"
     line << delimiter
 
     line << FFaker::Internet.email  #=> "kirsten.greenholt@corkeryfisher.info"
